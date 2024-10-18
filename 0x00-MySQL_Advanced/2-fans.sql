e15fb85a21af0d806bddbd863c23ef8fd6ad19bc
Ahmed Rady <rady20991@gmail.com>
2024-10-18 03:09:41 +0300

    Best band ever!

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -4,5 +4,5 @@
 -- Column names must be: origin and nb_fans
 -- Your script can be executed on any database
 
-SELECT origun, SUM(nb_fans) as nb_fans from metal_bands
-ORDER BY nb_fans DESC;
+SELECT origin, SUM(fans) as nb_fans FROM metal_bands
+GROUP BY origin ORDER BY nb_fans DESC;
