d7e559ca4ab91c5bb28d28e0c79dc0e8a3a79a60
Ahmed Rady <rady20991@gmail.com>
2024-10-23 21:18:37 +0300

    Writing strings to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,6 +6,7 @@ from typing import Union
 
 
 class Cache:
+    """Represents an object for storing data in a Redis data storage"""
     def __init__(self):
         """Initialize the Redis client and flush the database"""
         self._redis = redis.Redis()
