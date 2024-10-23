e22971860791b001e83f010f1c6932b27355c7aa
Ahmed Rady <rady20991@gmail.com>
2024-10-24 01:00:09 +0300

    Storing lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,6 +18,23 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """store the history of inputs and outputs for a particular function"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Add its input parameters to one list in redis,
+        and store its output into another list."""
+        in_key = f"{method.__qualname__}:inputs"
+        out_key = f"{method.__qualname__}:outputs"
+        if isinstance(self._redis, redis.Redis):
+            self._redis.rpush(in_key, str(args))
+        output = method(self, *args, **kwargs)
+        if isinstance(self._redis, redis.Redis):
+            self._redis.rpush(out_key, output)
+        return output
+    return wrapper
+
+
 class Cache:
     """Represents an object for storing data in a Redis data storage"""
 
@@ -27,6 +44,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, int, float, bytes]) -> str:
         """Store the input data in Redis using a randomly generated key"""
         key = str(uuid.uuid4())
